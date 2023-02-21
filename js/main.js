339fb8f3fe956f445454540f6b0b2c595a83d663
Carmine Faella <carminefaella5@gmail.com>
2023-02-21 15:22:14 +0100

    Fine Lavoro

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const nomeUtente = prompt("Inserire nome");
+
+const cognomeUtente = prompt("Inserire cognome");
+
+const colorePreferito = prompt("Inserire Colore Preferito");
+
+document.getElementById("info").innerHTML = `${nomeUtente}${cognomeUtente}${colorePreferito}23` 
